5e5b4ddb3ba890b7bc9c9d2e25f4de77e28c7070
jessiRob <69654419+jessiRob@users.noreply.github.com>
2021-11-14 19:58:37 -0500

    Delete bancAndes directory

--- bancAndes/data/SelectData.sql
+++
@@ -1,3 +0,0 @@
-SELECT * FROM bancAndes;
-
-SELECT * FROM Usuario;
--- bancAndes/src/main/java/uniandes/isis2304/bancAndes/negocio/VOUnicuenta.java
+++
@@ -1,7 +0,0 @@
-package uniandes.isis2304.bancAndes.negocio;
-
-public interface VOUnicuenta {
-	public long getId();
-	
-	public String getTipo();
-}
